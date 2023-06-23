2a5fe48974c85051fe1e28731b4e130651351636
ahmedkaimkhani <ak9302359@gmail.com>
2023-06-24 01:38:11 +0500

    update list % map.dart

---
+++ Class 5/list & map.dart
@@ -0,0 +1,10 @@
+void main (){
+  List studentsName = ['Ahmed', 'Usman'];
+
+  Map students1Records = {
+    "name" : ['Ahmed','Usman'],
+    "rollNo" : [20, 22]
+  };
+  print(students1Records["name"][1]);
+  print(students1Records["rollNo"][1]);
+}
