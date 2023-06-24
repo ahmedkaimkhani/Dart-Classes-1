a85304743dc113c37aa1b3e8f7f1c8eebcaed243
ahmedkaimkhani <ak9302359@gmail.com>
2023-06-25 02:35:47 +0500

    update add method, putIfAbsent ,

--- Class 5/add_method.dart
+++ Class 5/add_method.dart
@@ -4,6 +4,11 @@ void main (){
     2 : 'Orange',
     3 : 'Mango',
   };
+  // you can add or update in this method
   fruit [4] = 'Banana';
   print(fruit);
+
+  // putIfAbsent method if key don't exist in map than add, if exist than show previous one
+  var val = fruit.putIfAbsent(3, () => 'Banana');
+  print(val);
 }
