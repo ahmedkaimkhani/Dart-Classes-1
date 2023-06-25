dc5d9a0d59f0dfc2454c5eaa17b21202d00d5f0d
ahmedkaimkhani <ak9302359@gmail.com>
2023-06-25 21:07:24 +0500

    understanding merge method

--- Class 5/merge_method.dart
+++ Class 5/merge_method.dart
@@ -3,7 +3,13 @@ void main (){
   var f2 = {3: 'Banana'};
   var f3 = {4: 'Mango'};
 
-  // here we doing 
+  // here we doing merge all maps in new map throgh merge method
   var fruit = {} ..addAll(f1) ..addAll(f2) ..addAll(f3);
   print(fruit);
+
+  // OR
+  
+  // here we doing merge all maps in new map throgh merge method
+  var fruit2 = {...(f1), ...(f2), ...(f3)};
+  print(fruit2);
 }
