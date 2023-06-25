3306ee23bb7a80d1c89f9a5c7779d56af7902e03
ahmedkaimkhani <ak9302359@gmail.com>
2023-06-25 16:05:39 +0500

    understanding add method with different properties

--- Class 5/add_method.dart
+++ Class 5/add_method.dart
@@ -1,14 +1,17 @@
 void main (){ 
   var fruit = {
-    1 : 'Apple',
-    2 : 'Orange',
-    3 : 'Mango',
+    1 : 'Ahmed',
+    2 : 'Usman',
+    3 : 'Asim',
   };
   // you can add or update in this method
   // fruit [4] = 'Banana';
   // print(fruit);
 
-  // putIfAbsent method if key don't exist in map than add, if exist than show previous one
-   var abc = fruit.putIfAbsent(3, () => 'Banana');
+  // putIfAbsent method if key don't exist in map than add, if exist than print previous one
+   var abc = fruit.putIfAbsent(3, () => 'Ali');
   print(abc);
+
+  var abc2 = fruit.putIfAbsent(5, () => 'Hamza');
+  print(abc2);
 }
