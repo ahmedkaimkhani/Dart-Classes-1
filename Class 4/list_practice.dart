6c85ae8be82e6e3148941372369f1131ce9b13dd
ahmedkaimkhani <ak9302359@gmail.com>
2023-07-09 04:18:08 +0500

    List Practice

--- Class 4/list_practice.dart
+++ Class 4/list_practice.dart
@@ -1,5 +1,18 @@
 void main (){
-  var studentsNames = ['Ahmed', 'Usman', 'Asim', 'Bazil'];
+  // List<String> we just use String in list 
+  List<String> studentsNames = ['Ahmed', 'Usman', 'Asim', 'Bazil'];
 
-  print(studentsNames[0]);
+  print(studentsNames.length);
 }
+
+  // var datatype
+  // var studentsNames = ['Ahmed', 'Usman', 'Asim', 'Bazil', 4, true];
+  // print(studentsNames[0]);
+
+    // List datatype
+  // List studentsNames = ['Ahmed', 'Usman', 'Asim', 'Bazil', 4, true];
+  // print(studentsNames[5]);
+
+    // List<String> we just use String in list 
+  // List<String> studentsNames = ['Ahmed', 'Usman', 'Asim', 'Bazil'];
+  // print(studentsNames[5]);
