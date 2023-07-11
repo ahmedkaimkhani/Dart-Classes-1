5ae37cbe9aea60d768c7fca5f01e335732a9fc57
ahmedkaimkhani <ak9302359@gmail.com>
2023-07-12 04:26:49 +0500

    do whilw loop

--- Class 7/while_loop.dart
+++ Class 7/while_loop.dart
@@ -1,13 +1,13 @@
 import 'dart:io';
 
 void main (){
-  //  var email ;
-  //  var password ;
+   var email ;
+   var password ;
    
    bool login = false;
    while(login == false){
-    var email = stdin.readLineSync()!;
-    var password = stdin.readLineSync()!;
+    email = stdin.readLineSync()!;
+    password = stdin.readLineSync()!;
     if (email == 'ahmed@gmail.com' && password == '123456'){
     print('Login Succesful');
     // you can use here to stop loop, break; or change condition of loop, as i do
