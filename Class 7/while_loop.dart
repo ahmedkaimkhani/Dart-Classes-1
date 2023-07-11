0b62aa35f93f35f0ea39460f601975b048007c00
ahmedkaimkhani <ak9302359@gmail.com>
2023-07-12 03:38:39 +0500

    while loop

--- Class 7/while_loop.dart
+++ Class 7/while_loop.dart
@@ -1,16 +1,18 @@
 import 'dart:io';
 
 void main (){
-   var email ;
-   var password ;
+  //  var email ;
+  //  var password ;
    
    bool login = false;
    while(login == false){
-    email = stdin.readLineSync()!;
-    password = stdin.readLineSync()!;
+    var email = stdin.readLineSync()!;
+    var password = stdin.readLineSync()!;
     if (email == 'ahmed@gmail.com' && password == '123456'){
     print('Login Succesful');
-    login = true;
+    // you can use here to stop loop, break; or change condition of loop, as i do
+    break;
+    // login = true;
    } else {
     print('Login Failed');
    }
@@ -44,10 +46,30 @@ void main (){
   //  while(login == false){
   //   if (email == 'ahmed@gmail.com' && password == '123456'){
   //   print('Login Succesful');
+  // you can use here to stop loop, break; or change condition of loop, as i do
   //   login = true;
   //  } else {
   //   print('Login Failed');
   //   email = stdin.readLineSync();
   //   password = stdin.readLineSync();
+  //  }
+  //  }
+
+  // (4)
+// take userinput in while and it'll do same work
+  // var email ;
+  //  var password ;
+   
+  //  bool login = false;
+  //  while(login == false){
+  //   email = stdin.readLineSync()!;
+  //   password = stdin.readLineSync()!;
+  //   if (email == 'ahmed@gmail.com' && password == '123456'){
+  //   print('Login Succesful');
+  //   // you can use here to stop loop, break; or change condition of loop, as i do
+  //   break;
+  //   // login = true;
+  //  } else {
+  //   print('Login Failed');
   //  }
   //  }
