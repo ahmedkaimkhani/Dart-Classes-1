49b04107552684f59541395c5914de8fa29943e4
ahmedkaimkhani <ak9302359@gmail.com>
2023-07-11 20:36:01 +0500

    Nested Loop

--- Class 7/nested_loop.dart
+++ Class 7/nested_loop.dart
@@ -1,8 +1,21 @@
-void main (){
-  int abc = 0;
+import 'dart:io';
 
-  while(abc < 5){
-    print(abc);
-    abc++;
-  }
+void main (){
+  // we use int.parse to plus+ 2+3 =5 values if we don't use use int.parse it'll concadinate 2+3=23.
+  var email = stdin.readLineSync();
+   var password = stdin.readLineSync();
+   
+   if ((email == 'ahmed@gmail.com') && (password == '123456')){
+    print('Login Succesful');
+   } else {
+    print('Login Failed');
+   }
 }
+
+// While Loop
+// simple understanding for while loop with giving range
+// int abc = 0;
+//   while(abc < 5){
+//     print(abc);
+//     abc++;
+//   }
