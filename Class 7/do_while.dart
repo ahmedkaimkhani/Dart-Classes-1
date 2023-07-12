99ec51a7b736b5f5eaa43591e4bf9513bc0bf15c
ahmedkaimkhani <ak9302359@gmail.com>
2023-07-12 06:45:17 +0500

    Nested loop

--- Class 7/do_while.dart
+++ Class 7/do_while.dart
@@ -11,3 +11,11 @@ void main (){
 //   while(abc > 5){
 //     print('Ahmed');
 //   }
+
+// do while loop mustly run 1 time then check condition if condition
+// is true than do while loop continou utill condition is not false
+// Exapmle 
+  // int abc = 0;
+//  do {
+//    print('Ahmed');
+//  } while (abc > 5);
--- Class 7/nested_loop.dart
+++ Class 7/nested_loop.dart
@@ -1,3 +1,8 @@
 void main (){
-  
+  for (int i = 0; i < 5; i++){
+    for (int j = 0; j < 10; j++){
+      print('$i = $j');
+    }
+    print('======');
+  }
 }
