0cc895bc5fc9bea4b4b9bff5c78c7573e19cb3aa
ahmedkaimkhani <ak9302359@gmail.com>
2023-07-12 17:04:35 +0500

    Nested For Loop Miltiple Tables

--- Class 7/nested_loop.dart
+++ Class 7/nested_loop.dart
@@ -1,8 +1,17 @@
 void main (){
-  for (int i = 0; i < 5; i++){
-    for (int j = 0; j < 10; j++){
-      print('$i = $j');
+  for (int i = 1; i < 5; i++){
+    for (int j = 1; j <= 10; j++){
+      print('$i x $j = ${i*j}');
     }
-    print('======');
+    print('=========');
   }
+  print('End');
 }
+
+// Nested Loop Method to understand
+// for (int i = 0; i < 5; i++){
+//     for (int j = 0; j < 10; j++){
+//       print('$i = $j');
+//     }
+//     print('======');
+//   }
