573d601cab7bd3f4ca8e2345e306af6f024cc9c7
ahmedkaimkhani <ak9302359@gmail.com>
2023-07-13 02:45:44 +0500

    Functions

--- Class 8/fuction.dart
+++ Class 8/fuction.dart
@@ -5,11 +5,11 @@ void main (){
 
 studentRecords (){
   String name = 'Ahmed';
-  int subject1 = 77;
-  int subject2 = 89;
-  int subject3 = 69;
-  int totalMarks = 300;
-  int obtainMarks = (subject1 + subject2 + subject3);
+  num subject1 = 77;
+  num subject2 = 89;
+  num subject3 = 69;
+  num totalMarks = 300;
+  num obtainMarks = (subject1 + subject2 + subject3);
   var percentage = (obtainMarks / totalMarks) * 100;
 
   if (percentage > 50){
