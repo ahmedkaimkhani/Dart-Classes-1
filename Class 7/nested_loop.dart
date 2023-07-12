618af4829ac62e329ee27b234ef5afde3a931c91
ahmedkaimkhani <ak9302359@gmail.com>
2023-07-12 17:17:38 +0500

    Functions

--- Class 7/nested_loop.dart
+++ Class 7/nested_loop.dart
@@ -15,3 +15,17 @@ void main (){
 //     }
 //     print('======');
 //   }
+
+// Nested For Loop Multiples Table
+// i itration start run with 1
+// for (int i = 1; i < 5; i++){
+  // j itration run 10 times for (1 of i), than 10 times for (2 of i), until the i condition will false
+//     for (int j = 1; j <= 10; j++){
+  //  print multipes time in j nested loop
+//       print('$i x $j = ${i*j}');
+//     }
+// print in main for loop after complete j itration
+//     print('=========');
+//   }
+// print in main {} outside of the loop for end
+//   print('End');
