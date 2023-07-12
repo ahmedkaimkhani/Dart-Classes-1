3083417aec9cd264c2a3467fcd568c0b15a42faa
ahmedkaimkhani <ak9302359@gmail.com>
2023-07-13 03:04:25 +0500

    Functions

--- Class 8/fuction.dart
+++ Class 8/fuction.dart
@@ -1,22 +1,35 @@
 void main (){
-  var abc = studentRecords();
+  var abc = sumOfTwoNumbers();
   print('value of abc is: $abc');
 }
 
-studentRecords (){
-  String name = 'Ahmed';
-  num subject1 = 77;
-  num subject2 = 89;
-  num subject3 = 69;
-  num totalMarks = 300;
-  num obtainMarks = (subject1 + subject2 + subject3);
-  var percentage = (obtainMarks / totalMarks) * 100;
-
-  if (percentage > 50){
-    print('$name Pass');
-    return '$name Pass';
-  } else {
-    print('$name Fail');
-    return '$name Fail';
-  }
+sumOfTwoNumbers (){ 
+  num num1 = 5;
+  num num2 = 7;
+  return num1 + num2;
 }
+
+
+// understanding functions student record
+// void main (){
+//   var abc = studentRecords();
+//   print('value of abc is: $abc');
+// }
+
+// studentRecords (){
+//   String name = 'Ahmed';
+//   num subject1 = 77;
+//   num subject2 = 89;
+//   num subject3 = 69;
+//   num totalMarks = 300;
+//   num obtainMarks = (subject1 + subject2 + subject3);
+//   var percentage = (obtainMarks / totalMarks) * 100;
+
+//   if (percentage > 50){
+//      print('$name Pass');
+//     return '$name Pass';
+//   } else {
+//     print('$name Fail');
+//     return '$name Fail';
+//   }
+// }
