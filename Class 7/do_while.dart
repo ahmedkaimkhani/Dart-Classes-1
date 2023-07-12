6ab988b6f8833a45bce4d76184ac81b9a79a7234
ahmedkaimkhani <ak9302359@gmail.com>
2023-07-12 06:30:44 +0500

    Do While loop

---
+++ Class 7/do_while.dart
@@ -0,0 +1,13 @@
+void main (){
+  int abc = 0;
+ do {
+   print('Ahmed');
+ } while (abc > 5);
+}
+
+// while run when condition will be true
+// Example
+//  int abc = 0;
+//   while(abc > 5){
+//     print('Ahmed');
+//   }
