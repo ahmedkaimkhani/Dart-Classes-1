d4b0e08d8794a845be4ad13d58dc55e6e0828dfd
ahmedkaimkhani <ak9302359@gmail.com>
2023-07-13 23:53:23 +0500

    Marksheet

--- Class 8/function_marksheet.dart
+++ Class 8/function_marksheet.dart
@@ -1,5 +1,13 @@
 void main (){
-
+  markSheet(
+    name: 'Ahmed', 
+    rollno: 05, 
+    english: 70, 
+    urdu: 63, 
+    math: 87, 
+    physics: 72, 
+    chemistry: 81
+    );
 }
 
 markSheet ({
