5b9e1eec984470d9b84e7cdf04d68d5be7fa1012
ahmedkaimkhani <ak9302359@gmail.com>
2023-07-13 23:41:25 +0500

    Functions

--- Class 8/function_marksheet.dart
+++ Class 8/function_marksheet.dart
@@ -1,3 +1,39 @@
 void main (){
 
 }
+
+markSheet ({
+  required String name, 
+  required num rollno,
+  required num english,
+  required num urdu,
+  required num math,
+  required num physics,
+  required num chemistry
+  })
+  { 
+    num obtainMarks = 0;
+    String grade = '';
+    num totalMarks = 500;
+    var percentage = (obtainMarks / totalMarks) * 100;
+
+    if (percentage >= 80){
+      grade = 'A';
+      return grade;
+    } else if (percentage >= 70){
+      grade = 'B';
+      return grade;
+    } else if (percentage >= 60){
+      grade = 'C';
+      return grade;
+    } else if (percentage >= 60){
+      grade = 'D';
+      return grade;
+    } else if (percentage >= 50){
+      grade = 'E';
+      return grade;
+    } else {
+      return 'Fail';
+    }
+
+}
