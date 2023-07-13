7b2c1745b273b4272a68e7f103522429504b21a6
ahmedkaimkhani <ak9302359@gmail.com>
2023-07-14 04:04:46 +0500

    Loops

--- Class 7/nested_loop.dart
+++ Class 7/nested_loop.dart
@@ -4,14 +4,14 @@ void main (){
   num number = 4;
   for (int i = 1; i <= number; i++){
     for(int j = 1; j <= i; j++){
-      stdout.write('*');
+      stdout.write(i);
     }
     print('');
   }
 }
 
 // Nested Loop Method to understand
-// for (int i = 0; i < 5; i++){
+//1: for (int i = 0; i < 5; i++){
 //     for (int j = 0; j < 10; j++){
 //       print('$i = $j');
 //     }
@@ -20,7 +20,7 @@ void main (){
 
 // Nested For Loop Multiples Table
 // i itration start run with 1
-// for (int i = 1; i < 5; i++){
+//2; for (int i = 1; i < 5; i++){
   // j itration run 10 times for (1 of i), than 10 times for (2 of i), until the i condition will false
 //     for (int j = 1; j <= 10; j++){
   //  print multipes time in j nested loop
@@ -31,3 +31,16 @@ void main (){
 //   }
 // print in main {} outside of the loop for end
 //   print('End');
+
+//3:   num number = 4;
+  // for (int i = 1; i <= number; i++){
+  //   for(int j = 1; j <= i; j++){
+  //     stdout.write('*');
+  //   }
+  //   print('');
+  // }
+  // output:
+  // *
+  // **
+  // ***
+  // ****
