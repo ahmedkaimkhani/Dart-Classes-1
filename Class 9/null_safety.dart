8a4c770555d33d1a7727d59713a863d3dbb1c194
ahmedkaimkhani <ak9302359@gmail.com>
2023-07-16 17:27:40 +0500

    nullsafty

--- Class 9/null_safety.dart
+++ Class 9/null_safety.dart
@@ -1,3 +1,11 @@
+import 'dart:io';
+
 void main (){
-  
+  // Null Safty,
+
+  //(1) ! exclamation mark, iska matlab jahan bhi ! use krogay woh program ko make sure kryge
+  // kay yahan data lazmi aayega , or ager data nh aaya to run time per error aayega program crash ho jayega.
+
+  stdout.write('Enter for !: ');
+  String input = (stdin.readLineSync()!);
 }
