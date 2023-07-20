5225af21d0d43913d4f5ddc65b0c817c01b4fd3b
ahmedkaimkhani <ak9302359@gmail.com>
2023-07-21 03:46:15 +0500

    null safty

--- Class 9/null_safety.dart
+++ Class 9/null_safety.dart
@@ -1,11 +1,18 @@
 import 'dart:io';
 
 void main (){
+
+}
+
   // Null Safty,
 
   //(1) ! exclamation mark, iska matlab jahan bhi ! use krogay woh program ko make sure kryge
-  // kay yahan data lazmi aayega , or ager data nh aaya to run time per error aayega program crash ho jayega.
+  // kay yahan data lazmi aayega , or ager data nh aaya to run time per error aayega program 
+  // crash ho jayega. or yeh hmesha initialize variable kay sath use hoga
 
-  stdout.write('Enter for !: ');
-  String input = (stdin.readLineSync()!);
-}
+  //(2) ? question mark, ap wahan use krogay jahan ap sure nh ho kay yahan value ayega 
+  // ya nh ? ka matlab yahan value aa bhi sakti or null bhi ho sakta hai.
+
+  //(3) ?? double question mark, yeh almost if else he hai, ?? jis tarha hum if else
+  // me check krty hain ager ek variable me value aa rahi tw print krdo ya use krlo
+  // otherwise ?? yeh value lelo.
