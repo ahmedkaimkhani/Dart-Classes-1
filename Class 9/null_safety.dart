6dd465055274498872669f178bed4e80ba91575f
ahmedkaimkhani <ak9302359@gmail.com>
2023-07-21 15:46:49 +0500

    null safty practice

--- Class 9/null_safety.dart
+++ Class 9/null_safety.dart
@@ -1,12 +1,13 @@
 
 void main (){
-  sumOfTwonumbers(name: 'Usman');
+  sumOfTwonumbers();
 }
 
- sumOfTwonumbers ({String? name, String? fbLink}){
-  String? abc = name ?? 'Ahmed';
-  String bca = fbLink ?? 'http:facebook.com';
-  print('${name!+fbLink!}');
+ sumOfTwonumbers ({num? num1, num? num2}){
+  num number1 = num1 ?? 5;
+  num number2 = num2 ?? 4;
+  num result = number1 + number2;
+  print('Result of Number1 + Number2 is: $result');
 
  }
   // Null Safty,
@@ -86,3 +87,17 @@ void main (){
 //   String? officeKaCo = resumeKoKaamAttaHaiYaNahi ?? "Ahmed";
 //   print(officeKaCo);
 //  }
+
+//(7): practice ??
+
+// void main (){
+//   sumOfTwonumbers(name: 'Usman');
+// }
+
+//  sumOfTwonumbers ({String? name, String? fbLink}){
+//   String? abc = name ?? 'Ahmed';
+//   String bca = fbLink ?? 'http:facebook.com';
+//   print('${abc +" "+ bca}');
+
+//  }
+// output: Usman http:facebook.com
