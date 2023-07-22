578122d281f7ecf67d1d850bcaf8ed288fbf2b4d
ahmedkaimkhani <ak9302359@gmail.com>
2023-07-22 17:13:23 +0500

    classes

---
+++ Class 11/practice_classes.dart
@@ -0,0 +1,20 @@
+void main (){
+  Student student1 = Student();
+  student1.name = 'Ahmed';
+  student1.rollNo = 10;
+  print('Name: ${student1.name} and Roll Number: ${student1.rollNo}');
+  student1.study();
+}
+
+class Student {
+   int rollNo = 0;
+   String name = '';
+
+   study (){
+    print('$name is studying');
+   }
+
+   sleep (){
+
+   }
+}
