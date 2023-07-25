47efe0078fa84347c9053d741589d127500125c9
ahmedkaimkhani <ak9302359@gmail.com>
2023-07-26 04:39:33 +0500

    enum

--- Class 11/enum.dart
+++ Class 11/enum.dart
@@ -1,19 +1,60 @@
-enum PaymentType {EasyPaisa, JazzCash, HBL, UBL, CashOnDelivery}
-void main (){
-  var paymentMethod = PaymentType.EasyPaisa;
+enum PaymentType { EasyPaisa, JazzCash, HBL, UBL, CashOnDelivery }
+
+void main() {
+  var paymentMethod = PaymentType.CashOnDelivery;
 
   switch (paymentMethod) {
     case PaymentType.EasyPaisa:
-      print('${paymentMethod.toString().split('.').last} is selected for payment method');
+      print(
+          '${paymentMethod.toString().split('.').last} is selected for payment method');
       break;
-      case PaymentType.HBL:
-      print('$paymentMethod is selected for payment method');
+    case PaymentType.HBL:
+      print(
+          '${paymentMethod.toString().split('.').last} is selected for payment method');
       break;
-      case PaymentType.CashOnDelivery:
-      print('$paymentMethod is selected for payment method');
+    case PaymentType.UBL:
+      print(
+          '${paymentMethod.toString().split('.').last} is selected for payment method');
+      break;
+    case PaymentType.JazzCash:
+      print(
+          '${paymentMethod.toString().split('.').last} is selected for payment method');
       break;
     default:
-    print('Select payment method');
-    break;
+      print(
+          '${paymentMethod.toString().split('.').last} is selected for payment method');
+      break;
   }
 }
+
+
+//(1) enum with switch case
+// enum PaymentType { EasyPaisa, JazzCash, HBL, UBL, CashOnDelivery }
+
+// void main() {
+//   var paymentMethod = PaymentType.CashOnDelivery;
+
+//   switch (paymentMethod) {
+//     case PaymentType.EasyPaisa:
+//       print(
+//           '${paymentMethod.toString().split('.').last} is selected for payment method');
+//       break;
+//     case PaymentType.HBL:
+//       print(
+//           '${paymentMethod.toString().split('.').last} is selected for payment method');
+//       break;
+//     case PaymentType.UBL:
+//       print(
+//           '${paymentMethod.toString().split('.').last} is selected for payment method');
+//       break;
+//     case PaymentType.JazzCash:
+//       print(
+//           '${paymentMethod.toString().split('.').last} is selected for payment method');
+//       break;
+//     default:
+//       print(
+//           '${paymentMethod.toString().split('.').last} is selected for payment method');
+//       break;
+//   }
+// }
+
