9e2204c4a1c90d1a4ace5a637fab36a1fd75c86e
ahmedkaimkhani <ak9302359@gmail.com>
2023-07-25 15:53:24 +0500

    class11

--- Class 11/classes.dart
+++ Class 11/classes.dart
@@ -2,6 +2,8 @@ void main (){
   Human obj = Human();
   print(obj.name);
   obj.eat();
+  obj.sleep();
+  obj.coding();
 }
 
 class Human {
@@ -12,7 +14,39 @@ class Human {
     print('$name is eating');
   }
 
-  sleep (){}
+  sleep (){
+    print('$name is sleeping');
+  }
 
-  coding (){}
+  coding (){
+    print('$name in doing some code');
+  }
 }
+
+// Understanding Classes 
+
+//(1);
+// void main (){
+//   Human obj = Human();
+//   print(obj.name);
+//   obj.eat();
+//   obj.sleep();
+//   obj.coding();
+// }
+
+// class Human {
+//   String name = 'Ahmed';
+//   String age = '21';
+
+//   eat (){
+//     print('$name is eating');
+//   }
+
+//   sleep (){
+//     print('$name is sleeping');
+//   }
+
+//   coding (){
+//     print('$name in doing some code');
+//   }
+// }
