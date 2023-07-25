ae0b4f76557a36eec38edcee488f295fb826f594
ahmedkaimkhani <ak9302359@gmail.com>
2023-07-25 16:45:08 +0500

    class 11

--- Class 11/classes.dart
+++ Class 11/classes.dart
@@ -1,25 +1,29 @@
-void main (){
+void main() {
   Human obj = Human();
-  print(obj.name);
-  obj.eat();
-  obj.sleep();
-  obj.coding();
+  Human obj1 = Human();
+  Human obj2 = Human();
+
+  obj.name = 'Usman';
+  obj.age = '22';
+  obj.rollNo = '100';
+
+  print('=======');
+  obj.humanInfo();
+  print('=======');
+  obj1.humanInfo();
+  print('=======');
+  obj2.humanInfo();
 }
 
 class Human {
   String name = 'Ahmed';
   String age = '21';
+  String rollNo = '10';
 
-  eat (){
-    print('$name is eating');
-  }
-
-  sleep (){
-    print('$name is sleeping');
-  }
-
-  coding (){
-    print('$name in doing some code');
+  humanInfo (){
+    print('Name: $name');
+    print('Age: $age');
+    print('Roll No: $rollNo');
   }
 }
 
@@ -50,3 +54,29 @@ class Human {
 //     print('$name in doing some code');
 //   }
 // }
+
+//(2)
+// void main() {
+//   Human obj = Human();
+// we update name here and the function recall
+//   obj.name = 'Usman';
+//   obj.coding();
+// }
+
+// class Human {
+//   String name = 'Ahmed';
+//   String age = '21';
+
+//   coding() {
+//     print('$name in doing some code');
+//   }
+
+//   eat() {
+//     print('$name is eating');
+//   }
+
+//   sleep() {
+//     print('$name is sleeping');
+//   }
+// }
+// Output: Usman in doing some code
