c9f7c2f358349b5f8053702b0f1ab4b2a6c107af
ahmedkaimkhani <ak9302359@gmail.com>
2023-07-26 03:04:51 +0500

    class 11

---
+++ Class 11/switch_case.dart
@@ -0,0 +1,18 @@
+import 'dart:io';
+
+void main (){
+  num num1 = 3;
+  num num2 = 4;
+
+  var userInput = stdin.readLineSync();
+
+  switch (userInput) {
+    case '+':
+      print(num1 + num2);
+      break;
+      case '-':
+      print(num1 - num2);
+      break;
+    default:
+  }
+}
