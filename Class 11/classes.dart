4a9531f9ab0c7704f087f23fcca81d3214aa6af7
ahmedkaimkhani <ak9302359@gmail.com>
2023-07-25 15:38:05 +0500

    class 11

--- Class 11/classes.dart
+++ Class 11/classes.dart
@@ -1,13 +1,16 @@
 void main (){
   Human obj = Human();
   print(obj.name);
+  obj.eat();
 }
 
 class Human {
   String name = 'Ahmed';
   String age = '21';
 
-  eat (){}
+  eat (){
+    print('$name is eating');
+  }
 
   sleep (){}
 
