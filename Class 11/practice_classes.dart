5445f5316a8e81f955fc11006e22e559d1cc3191
ahmedkaimkhani <ak9302359@gmail.com>
2023-07-26 16:03:26 +0500

    practice

--- Class 11/practice_classes.dart
+++ Class 11/practice_classes.dart
@@ -18,3 +18,26 @@ class Student {
 
    }
 }
+
+// (1)
+// void main (){
+//   Student student1 = Student();
+//   student1.name = 'Ahmed';
+//   student1.rollNo = 10;
+//   print('Name: ${student1.name} and Roll Number: ${student1.rollNo}');
+//   student1.study();
+// }
+
+// class Student {
+//    int rollNo = 0;
+//    String name = '';
+
+//    study (){
+//     print('$name is studying');
+//    }
+
+//    sleep (){
+
+//    }
+// }
+
