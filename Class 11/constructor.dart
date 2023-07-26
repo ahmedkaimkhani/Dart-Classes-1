3013069986e2ee231eab71953983047e03efb940
ahmedkaimkhani <ak9302359@gmail.com>
2023-07-26 16:41:47 +0500

    constructor

--- Class 11/constructor.dart
+++ Class 11/constructor.dart
@@ -3,8 +3,12 @@ void main (){
   object1.rollNo = 1;
   object1.name = 'Ahmed';
   print('Roll No ${object1.rollNo} and Name ${object1.name}');
+
   Student object2 = Student();
-  
+  object2.rollNo = 1;
+  object2.name = 'Ahmed';
+  print('Roll No ${object2.rollNo} and Name ${object2.name}');
+
 }
 
 class Student {
