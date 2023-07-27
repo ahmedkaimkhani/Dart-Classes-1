5161019337cda8b7df592cff618c966388b04ff9
ahmedkaimkhani <ak9302359@gmail.com>
2023-07-27 17:35:01 +0500

    classes

--- Class 11/inheritance.dart
+++ Class 11/inheritance.dart
@@ -49,3 +49,25 @@ class flutter extends Student {
 
 // There are three type of inheritance.
 // 1: Single Inheritance, 2: Multi-Level Inheritance and Hierarchical Inheritance.
+
+// 1: Single Inheritance.
+
+//(1)
+// void main (){
+//   flutter obj = flutter();
+//   obj.name = 'Ahmed';
+//   obj.rollNo = 110;
+//   print('Name: ${obj.name}\nRoll Number: ${obj.rollNo}');
+//   obj.batch();
+// }
+
+// class Student {
+//   late int rollNo;
+//   late String name;
+// }
+
+// class flutter extends Student {
+//   batch (){
+//     print('SMIT Batch: 7 to 9');
+//   }
+// }
