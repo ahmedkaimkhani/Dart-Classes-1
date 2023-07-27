2fc52489ed8b224042e4bdb806b3969015739072
ahmedkaimkhani <ak9302359@gmail.com>
2023-07-28 04:41:57 +0500

    abstract

--- Class 11/abstract.dart
+++ Class 11/abstract.dart
@@ -22,3 +22,28 @@ class Square extends Shape {
     print('Draw Shape of Square');
   }
 }
+
+// Note: Abstract class ka kabhi object nh ban sakta.
+// void main (){
+//   Rectangle rect = Rectangle();
+//   rect.draw();
+//   Square sqr = Square();
+//   sqr.draw();
+  
+// }
+
+// abstract class Shape {
+//   void draw ();
+// }
+
+// class Rectangle extends Shape {
+//   draw (){
+//     print('Draw Shape of Rectangle');
+//   }
+// }
+
+// class Square extends Shape {
+//   void draw (){
+//     print('Draw Shape of Square');
+//   }
+// }
