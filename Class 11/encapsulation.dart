96088f485db36b54176a2f84263296ab6f040ad7
ahmedkaimkhani <ak9302359@gmail.com>
2023-07-30 03:19:23 +0500

    encapsulation

--- Class 11/encapsulation.dart
+++ Class 11/encapsulation.dart
@@ -1,9 +1,10 @@
 // 2: Encapsulation
 void main (){
   Student obj = Student();
-  obj._name = 'Ahmed';
- obj.setAge(21);
-  print(obj._name);
+  obj.setName('Ahmed');
+  obj.setAge(-21); // This will set age to 0
+  print(obj.getAge()); // Output: 0
+  print(obj.getName()); // Output: Ahmed
 }
 
 class Student {
@@ -36,3 +37,44 @@ class Student {
     }
   }
 }
+
+// 2: Encapsulation
+//(1)
+// void main (){
+//   Student obj = Student();
+//   obj.setName('Ahmed');
+//   obj.setAge(-21); // This will set age to 0
+//   print(obj.getAge()); // Output: 0
+//   print(obj.getName()); // Output: Ahmed
+// }
+
+// class Student {
+//   // private property
+//   late String _name;
+//   late int _age;
+
+//   // getter to get name
+//   String getName (){
+//     return this._name;
+//   }
+
+//   // Getter to get age
+//   int getAge (){
+//     return this._age;
+//   }
+
+//   // Setter to set name 
+//   void setName (String name){
+//     this._name = name;
+//   }
+
+//   // Setter to set age 
+//   void setAge (int age){
+//     // you can also use condition if else
+//     if (age < 0){
+//       this._age = 0;
+//     } else {
+//     this._age = age;
+//     }
+//   }
+// }
