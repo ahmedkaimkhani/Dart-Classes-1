c8349489119cceff7186281a077a566fc581079e
ahmedkaimkhani <ak9302359@gmail.com>
2023-07-30 01:54:08 +0500

    encapsulation

--- Class 11/encapsulation.dart
+++ Class 11/encapsulation.dart
@@ -6,4 +6,14 @@ class Student {
   // private property
   late String _name;
   late int _age;
+
+  // getter to get name
+  String getName (){
+    return this._name;
+  }
+
+  // Getter to get age
+  int setAge (){
+    return this._age;
+  }
 }
