fb0c90da53b97d9711ce6c5241649a8f1e709762
ahmedkaimkhani <ak9302359@gmail.com>
2023-07-31 01:28:35 +0500

    static

--- Class 11/Classes/static.dart
+++ Class 11/Classes/static.dart
@@ -1,17 +1,23 @@
 // you can't make object/instance where you use static variable or method
 // Static Variable & Static Method
 void main (){
-  Circle.pi = 3.5;
-  print(Circle.pi);
-  //print('Value of Pi: ${Circle.pi}');
-  Circle.calculateArea();
+  Circle circle = Circle();
+  circle.myNormalFUnction();
 }
 
+//create class
 class Circle {
-  static late double pi;
+  static double pi = 3.14;
 
+  // you can't call other method/function in static method
+  // but you can call static method in Normal Function.
   static void calculateArea (){
+    // myNormalFUnction();
     print('Static method Practice');
+  }
+  myNormalFUnction (){
+    calculateArea();
+    print('My Normal Function');
   }
 }
 
@@ -31,3 +37,23 @@ class Circle {
 //     print('Static method Practice');
 //   }
 // }
+
+//(2)
+// void main (){
+//   Circle.pi = 3.5;
+//   print(Circle.pi);
+//   //print('Value of Pi: ${Circle.pi}');
+//   Circle.calculateArea();
+// }
+
+// //create class
+// class Circle {
+//   static double pi = 3.14;
+
+//   static void calculateArea (){
+//     print('Static method Practice');
+//   }
+// }
+// Output:
+// 3.5
+// Static method Practice
