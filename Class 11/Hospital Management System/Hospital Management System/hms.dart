b542f8f8f6bfe5a946d843a25b565a21c40905b9
ahmedkaimkhani <ak9302359@gmail.com>
2023-08-01 04:33:23 +0500

    hms

--- Class 11/Hospital Management System/Hospital Management System/hms.dart
+++ Class 11/Hospital Management System/Hospital Management System/hms.dart
@@ -100,6 +100,26 @@ class PatientExist {
   }
 }
 
+class ViewPatientRecords {
+  // Patient View Records
+ViewPatientRecords() {
+  if (patientList.isEmpty) {
+    print('No Patient Data Found!');
+  } else {
+    print(' ***=== Patient Records ===***');
+    print('');
+    print(' Patients: ${patientList.length}');
+    print('');
+    for (int i = 0; i < patientList.length; i++) {
+      Map<String, dynamic> patientRecordExist = patientList[i];
+      print(
+          ' Id: ${patientRecordExist['id']}\n Name: ${patientRecordExist['name']}\n Age: ${patientRecordExist['age']}\n PhoneNo: ${patientRecordExist['phoneNo']}\n Gender: ${patientRecordExist['gender']}\n Address: ${patientRecordExist['address']}');
+      print('');
+    }
+  }
+}
+}
+
 
 
 
