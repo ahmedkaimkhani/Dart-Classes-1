072ccc786d33dcb2f29a56b75c72f8c47b413cba
ahmedkaimkhani <ak9302359@gmail.com>
2023-08-04 02:41:18 +0500

    oops

--- Class 11/Pillars Of Oops/inheritance.dart
+++ Class 11/Pillars Of Oops/inheritance.dart
@@ -14,33 +14,10 @@ class Animal {
   }
 }
 
+class Dog 
+
 
 // Inheritance
-// void main (){
-//   Dog dog = Dog();
-//   dog.name = 'Bazil';
-//   dog.age = 19;
-//   print('Dog name is ${dog.name} and age is ${dog.age}');
-//   dog.bark();
-// }
-
-// get access properties of a class in multiple classes 
-// class Animal {
-//   String? name;
-//   int? age;
-// }
-
-// class Dog extends Animal{
-//   void bark (){
-//     print('Dog is barking');
-//   }
-// }
-
-// class Cat extends Animal{
-//   void meow (){
-//     print('Cat is meowing');
-//   }
-// }
 
 // There are three type of inheritance.
 // 1: Single Inheritance, 2: Multi-Level Inheritance and Hierarchical Inheritance.
@@ -64,5 +41,31 @@ class Animal {
 // class flutter extends Student {
 //   batch (){
 //     print('SMIT Batch: 7 to 9');
+//   }
+// }
+
+// void main (){
+//   Dog dog = Dog();
+//   dog.name = 'Bazil';
+//   dog.age = 19;
+//   print('Dog name is ${dog.name} and age is ${dog.age}');
+//   dog.bark();
+// }
+
+// get access properties of a class in multiple classes 
+// class Animal {
+//   String? name;
+//   int? age;
+// }
+
+// class Dog extends Animal{
+//   void bark (){
+//     print('Dog is barking');
+//   }
+// }
+
+// class Cat extends Animal{
+//   void meow (){
+//     print('Cat is meowing');
 //   }
 // }
