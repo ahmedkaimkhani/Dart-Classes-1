8705d96d94fb526f148f26b192ac867a212977ce
ahmedkaimkhani <ak9302359@gmail.com>
2023-08-05 00:58:30 +0500

    mixin

--- Class 13/Mixin/advance_mixin.dart
+++ Class 13/Mixin/advance_mixin.dart
@@ -1,6 +1,17 @@
 // Note: You can’t create an object of mixin.
 // On keyword in mixin.
 
+import '../../Class 11/Pillars Of Oops/inheritance.dart';
+
 void main (){
-  
+
+}
+
+abstract class Human {
+  // Properties
+  late String name;
+  late int speed;
+
+  // constructor
+  Human (this.name, this.speed);
 }
