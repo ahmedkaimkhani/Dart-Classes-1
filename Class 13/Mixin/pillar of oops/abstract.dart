e621c7ac60e52d0edb90b2866b3aa157fbb62e95
ahmedkaimkhani <ak9302359@gmail.com>
2023-08-05 02:59:39 +0500

    abstrat

--- Class 13/Mixin/pillar of oops/abstract.dart
+++ Class 13/Mixin/pillar of oops/abstract.dart
@@ -1,3 +1,7 @@
+// You can’t create an object of an abstract class.
+// It can have both abstract and non-abstract methods.
+// It is used to define the behavior of a class that other classes can inherit.
+// Abstract method only has a signature and no implementation.
 void main (){
-  
+
 }
