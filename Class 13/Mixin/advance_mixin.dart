3e8f1adf9d97570a0c45bb338a7abe93caedc505
ahmedkaimkhani <ak9302359@gmail.com>
2023-08-05 01:20:42 +0500

    mixin

--- Class 13/Mixin/advance_mixin.dart
+++ Class 13/Mixin/advance_mixin.dart
@@ -1,13 +1,14 @@
 // Note: You can’t create an object of mixin.
 // On keyword in mixin.
 void main (){
-
+  Ahmed ahmed = Ahmed('Ahmed', 10.0);
+  ahmed.run();
 }
 
 abstract class Human {
   // Properties
   late String name;
-  late int speed;
+  late double speed;
 
   // constructor
   Human (this.name, this.speed);
@@ -15,15 +16,17 @@ abstract class Human {
   // Abstract method
   void run ();
 }
-
+// mixin CanRun is only used by class that extends Animal
 mixin CanRun on Human {
 
+  // implementation of abstract method
   @override
   void run() => print('$name is running at the speed $speed');
   
 }
 
 class Ahmed extends Human with CanRun {
-  Ahmed(String name, int speed): super(name, speed);
+  // constructor
+  Ahmed(String name, double speed): super(name, speed);
   
 }
