c524453d3efedddd3c5325289ee3fd728f856d7c
ahmedkaimkhani <ak9302359@gmail.com>
2023-08-06 04:47:02 +0500

    interface

--- Class 13/Interface/interface_adv.dart
+++ Class 13/Interface/interface_adv.dart
@@ -1,5 +1,15 @@
+// An interface is a contract that defines the capabilities of a class.
+// Dart has no keyword interface, but you can use class or abstract class to declare an interface.
+// Use abstract class to declare an interface.
+// A class can extend only one class but can implement multiple interfaces.
+// Using the interface, you can achieve multiple inheritance in Dart.
+// It is used to achieve abstraction
+// Most of the time, abstract class is used instead of concrete class to declare an interface.
+// You must have override method and properties in implement class
 void main (){
-
+  Rectangle rectangle = Rectangle(10, 20);
+  rectangle.area();
+  rectangle.perimeter();
 }
 
 abstract class Area {
