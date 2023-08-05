baf52784fc80037915ae92c46367e66bace9ca85
ahmedkaimkhani <ak9302359@gmail.com>
2023-08-06 04:44:39 +0500

    interface

--- Class 13/Interface/interface_adv.dart
+++ Class 13/Interface/interface_adv.dart
@@ -1,3 +1,34 @@
 void main (){
-  
+
+}
+
+abstract class Area {
+  // abstract method
+  void area ();
+}
+
+abstract class Perimeter {
+  // abstract method
+  void perimeter ();
+}
+
+// implements multiple interfaces
+class Rectangle implements Area, Perimeter {
+  // properties
+  late int lenght, breadth;
+  // construtor
+  Rectangle (this.lenght, this.breadth);
+
+  // implementation of area()
+  @override 
+  void area (){
+    print('The area of rectangle id ${lenght * breadth}');
+  }
+
+  // implementation of perimeter()
+  @override 
+  void perimeter (){
+    print('The perimeter of rectangle is ${2 * (lenght * breadth)}');
+  }
+
 }
