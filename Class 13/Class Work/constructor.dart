ad97bcfb55c0724393d350e696012af24954201f
ahmedkaimkhani <ak9302359@gmail.com>
2023-08-07 02:24:03 +0500

    const

--- Class 13/Class Work/constructor.dart
+++ Class 13/Class Work/constructor.dart
@@ -1,5 +1,8 @@
+// when you create obj/instance constructor is called.
+// constructor name must have same name with class .
 void main (){
-
+  // constructor is called
+  Person person =Person();
 }
 
 class Person {
