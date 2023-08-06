35c7107723cb006ff2f26a3bc0d361f42296296f
ahmedkaimkhani <ak9302359@gmail.com>
2023-08-07 04:43:22 +0500

    inherit

--- Class 13/Class Work/inheritance.dart
+++ Class 13/Class Work/inheritance.dart
@@ -1,3 +1,14 @@
 void main (){
-  
+
+}
+
+class Person {
+  // Properties/Attributes
+  String? name;
+  int? age;
+  // method/behavior
+  void dispalyInfo (){
+    print('Name : $name');
+    print('Age : $age');
+  }
 }
