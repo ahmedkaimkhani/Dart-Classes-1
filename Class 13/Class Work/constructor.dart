2a0a362cdfed7f6355a2f74d607d6e5beab405d1
ahmedkaimkhani <ak9302359@gmail.com>
2023-08-07 02:30:28 +0500

    const

--- Class 13/Class Work/constructor.dart
+++ Class 13/Class Work/constructor.dart
@@ -1,12 +1,32 @@
 // when you create obj/instance constructor is called.
 // constructor name must have same name with class .
+// this. object kay reference kay liye use kia jata hai.
 void main (){
-  // constructor is called
-  Person person =Person();
 }
 
 class Person {
-  Person (){
-    print('Im default constructor');
+  String? name;
+  int? age;
+
+  Person (name, age){
+    name = name;
+    age = age;
   }
 }
+
+
+
+// Default Constructor
+//(1)
+// void main (){
+//   // constructor is called
+//   Person person =Person();
+// }
+
+// class Person {
+
+//   // constructor
+//   Person (){
+//     print('Im default constructor');
+//   }
+// }
