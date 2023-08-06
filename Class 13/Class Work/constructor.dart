da6294113b17701f0113e4d558d0f1c86019c5c0
ahmedkaimkhani <ak9302359@gmail.com>
2023-08-07 03:43:33 +0500

    const

--- Class 13/Class Work/constructor.dart
+++ Class 13/Class Work/constructor.dart
@@ -2,19 +2,21 @@
 // constructor name must have same name with class .
 // this. object kay reference kay liye use kia jata hai.
 void main (){
-  Person person = Person('Ahmed', 10);
+  Person person = Person( age: 15);
+  person.display();
 }
 
 class Person {
   String? name;
   int? age;
 
-  Person (name, age){
-    // this. perameter ki value ko class ki properties/attributes me store krne kay liye
-    // use kia hai obj kay ref kay liye.
-    this.name = name;
-    this.age = age;
-    print('Name: $name, Age: $age');
+  // short way, this. hum perameter me he variable name kay sath use kr leinge.
+  // named & optional perameter
+  Person ({this.name = 'Ahmed', this.age}); // we can also set default value
+
+  display (){
+  print('Name: $name & Age: $age');
+
   }
 }
 
@@ -52,3 +54,60 @@ class Person {
 //     print('Name: $name and Age: $age');
 //   }
 // }
+// Output: Name: Ahmed and Age: 10
+
+//(2)
+// void main (){
+//   Person person = Person('Ahmed', 10);
+// }
+
+// class Person {
+//   String? name;
+//   int? age;
+
+//   Person (name, age){
+//     // this. perameter ki value ko class ki properties/attributes me store krne kay liye
+//     // use kia hai obj kay ref kay liye.
+//     this.name = name;
+//     this.age = age;
+//     print('Name: $name, Age: $age');
+//   }
+// }
+// Output: Name: Ahmed, Age: 10
+
+// (3)
+// void main (){
+//   Person person = Person('Ahmed', 10);
+//   print('Name: ${person.name} & Age: ${person.age} ');
+// }
+
+// class Person {
+//   String name;
+//   int age;
+
+//   // short way, this. hum perameter me he variable name kay sath use kr leinge.
+//   Person (this.name, this.age);
+// }
+// Output: Name: Ahmed & Age: 10
+
+// Named & Optional Perameter
+// (1)
+// void main (){
+//   Person person = Person(name: 'Ahmed', age: 15);
+//   person.display();
+// }
+
+// class Person {
+//   String? name;
+//   int? age;
+
+//   // short way, this. hum perameter me he variable name kay sath use kr leinge.
+//   // named & optional perameter
+//   Person ({this.name, this.age});
+
+//   display (){
+//   print('Name: $name & Age: $age');
+
+//   }
+// }
+// Output: Name: Ahmed & Age: 15
