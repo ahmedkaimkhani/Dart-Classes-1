a0f9ad9f4aeb1f8d3cedc94601fd7a1c145d1415
ahmedkaimkhani <ak9302359@gmail.com>
2023-08-07 05:10:20 +0500

    inherit

--- Class 13/Class Work/inheritance.dart
+++ Class 13/Class Work/inheritance.dart
@@ -5,7 +5,7 @@ void main (){
   ahmed.name = 'Ahmed';
   ahmed.age = 21;
   ahmed.dispalyInfo();
-  ahmed.displayStudent();
+  ahmed.displayAhmed();
 }
 // parent class
 class Person {
@@ -21,12 +21,18 @@ class Person {
 // child/sub class
 // inherit krne kay liye extent keyword use krty hain
 class Student extends Person {
+  // method/behavior
   displayStudent (){
     print('Student ka Function');
   }
 }
 // child/sub class
-class Ahmed extends Student {}
+class Ahmed extends Person {
+  // method/behavior
+  displayAhmed (){
+    print('Yeh Ahmed ka function hai');
+  }
+}
 
 
 // Single Inheritance
@@ -37,6 +43,7 @@ class Ahmed extends Student {}
 //   student.age = 21;
 //   student.dispalyInfo();
 //   student.displayStudent();
+//   ahmed.displayAhmed();
 // }
 // // parent class
 // class Person {
@@ -60,3 +67,43 @@ class Ahmed extends Student {}
 // Name : Ahmed
 // Age : 21
 // Student ka Function
+
+// Multi-Level Inheritance
+// void main (){
+//   Ahmed ahmed = Ahmed();
+//   ahmed.name = 'Ahmed';
+//   ahmed.age = 21;
+//   ahmed.dispalyInfo();
+//   ahmed.displayStudent();
+// }
+// // parent class
+// class Person {
+//   // Properties/Attributes
+//   String? name;
+//   int? age;
+//   // method/behavior
+//   void dispalyInfo (){
+//     print('Name : $name');
+//     print('Age : $age');
+//   }
+// }
+// // child/sub class
+// // inherit krne kay liye extent keyword use krty hain
+// class Student extends Person {
+//   // method/behavior
+//   displayStudent (){
+//     print('Student ka Function');
+//   }
+// }
+// // child/sub class
+// class Ahmed extends Student {
+//   // method/behavior
+//   displayAhmed (){
+//     print('Yeh Ahmed ka function hai');
+//   }
+// }
+// Output:
+// Name : Ahmed
+// Age : 21
+// Student ka Function
+// Yeh Ahmed ka function hai
