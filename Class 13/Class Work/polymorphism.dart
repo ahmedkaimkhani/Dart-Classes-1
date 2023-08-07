e24892d34208962fab44306dbb5a3a884b064f9e
ahmedkaimkhani <ak9302359@gmail.com>
2023-08-07 23:24:59 +0500

    polymorphism

--- Class 13/Class Work/polymorphism.dart
+++ Class 13/Class Work/polymorphism.dart
@@ -1,3 +1,44 @@
 void main (){
-  
+  Student student = Student();
+  student.dispalyInfo();
 }
+
+class Person {
+  // method
+  void dispalyInfo (){
+    print('Polymorphism practice');
+  }
+}
+
+class Student extends Person {
+  @override
+  void dispalyInfo() {
+    super.dispalyInfo();
+    print('Overriding in polymorphism');
+  }
+}
+
+//Polymorphism
+// (1)
+// void main (){
+//   Student student = Student();
+//   student.dispalyInfo();
+// }
+
+// class Person {
+//   // method
+//   void dispalyInfo (){
+//     print('Polymorphism practice');
+//   }
+// }
+
+// class Student extends Person {
+//   @override
+//   void dispalyInfo() {
+//     super.dispalyInfo();
+//     print('Overriding in polymorphism');
+//   }
+// }
+// Output:
+// Polymorphism practice
+// Overriding in polymorphism
