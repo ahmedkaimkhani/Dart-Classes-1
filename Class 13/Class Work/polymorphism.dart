aa91172101272ad52fc84e4fe0f344570ba15ffd
ahmedkaimkhani <ak9302359@gmail.com>
2023-08-08 02:07:33 +0500

    polymorphishm

--- Class 13/Class Work/polymorphism.dart
+++ Class 13/Class Work/polymorphism.dart
@@ -1,4 +1,5 @@
 void main (){
+  // you give arguments in constructor but out me yeh nh ayege 
   Student student = Student('Ahmed', 21);
   student.dispalyInfo();
 }
@@ -17,9 +18,14 @@ class Person {
 
 class Student extends Person {
   Student(super.name, super.age);
-  // you can also do inialize or default attributes
+  // ap super class kay attributes ko bhi override kr sakty or value set kr sakty
+  // tw ager ager ap constructor me arguments pass keringe tw output me override
+  // wali given value he aayege, ager ap override me value nh dety
+  // tw constructor arguments wali value run ho jayege. like this
   @override
-  String? get name => super.name;
+  String? get name => super.name = 'Usman';
+  @override
+  int? get age => super.age = 21;
 
   @override
   void dispalyInfo() {
@@ -74,6 +80,48 @@ class Student extends Person {
 
 // class Student extends Person {
 //   Student(super.name, super.age);
+
+//   @override
+//   void dispalyInfo() {
+//     super.dispalyInfo();
+//     print('Overriding in polymorphism');
+//     print('Name: $name & Age: $age');
+//   }
+// }
+// Output:
+// Polymorphism practice
+// Overriding in polymorphism
+// Name: Ahmed & Age: 21
+
+// (3)
+// void main (){
+//   // you give arguments in constructor but out me yeh nh ayege 
+//   Student student = Student('Ahmed', 21);
+//   student.dispalyInfo();
+// }
+
+// class Person {
+//   // Properties/attributes
+//   String? name;
+//   int? age;
+//   // constructor
+//   Person(this.name,this.age);
+//   // method
+//   void dispalyInfo (){
+//     print('Polymorphism practice');
+//   }
+// }
+
+// class Student extends Person {
+//   Student(super.name, super.age);
+//   // ap super class kay attributes ko bhi override kr sakty or value set kr sakty
+//   // tw ager ager ap constructor me arguments pass keringe tw output me override
+//   // wali given value he aayege, ager ap override me value nh dety
+//   // tw constructor arguments wali value run ho jayege. like this
+//   @override
+//   String? get name => super.name = 'Usman';
+//   @override
+//   int? get age => super.age = 21;
 
 //   @override
 //   void dispalyInfo() {
