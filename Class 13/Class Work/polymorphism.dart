af7454f31591ad932bf7ae01eaf8078847e6ba5b
ahmedkaimkhani <ak9302359@gmail.com>
2023-08-08 01:40:35 +0500

    poly

--- Class 13/Class Work/polymorphism.dart
+++ Class 13/Class Work/polymorphism.dart
@@ -1,5 +1,5 @@
 void main (){
-  Student student = Student();
+  Student student = Student('Ahmed', 21);
   student.dispalyInfo();
 }
 
@@ -22,6 +22,7 @@ class Student extends Person {
   void dispalyInfo() {
     super.dispalyInfo();
     print('Overriding in polymorphism');
+    print('Name: $name & Age: $age');
   }
 }
 
@@ -49,3 +50,36 @@ class Student extends Person {
 // Output:
 // Polymorphism practice
 // Overriding in polymorphism
+
+// (2)
+// void main (){
+//   Student student = Student('Ahmed', 21);
+//   student.dispalyInfo();
+// }
+
+// class Person {
+//   // Properties/attributes
+//   String? name;
+//   int? age;
+//   // constructor
+//   Person(this.name,this.age);
+//   // method
+//   void dispalyInfo (){
+//     print('Polymorphism practice');
+//   }
+// }
+
+// class Student extends Person {
+//   Student(super.name, super.age);
+
+//   @override
+//   void dispalyInfo() {
+//     super.dispalyInfo();
+//     print('Overriding in polymorphism');
+//     print('Name: $name & Age: $age');
+//   }
+// }
+// Output:
+// Polymorphism practice
+// Overriding in polymorphism
+// Name: Ahmed & Age: 21
