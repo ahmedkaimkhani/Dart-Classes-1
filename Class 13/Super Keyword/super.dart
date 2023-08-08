bef621175ad205e7c35eb5f738394d32e05decbb
ahmedkaimkhani <ak9302359@gmail.com>
2023-08-09 00:51:29 +0500

    super

---
+++ Class 13/Super Keyword/super.dart
@@ -0,0 +1,12 @@
+// 1: The super keyword is used to access the parent class members.
+// 2: The super keyword is used to call the method of the parent class.
+void main (){
+
+}
+
+class Laptop {
+  // Method / Behavior
+  void show (){
+    print('Laptop show method');
+  }
+}
